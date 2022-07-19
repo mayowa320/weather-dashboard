4380ea336871d757c9fd086c6b0ad437876f7521
Mayowa <mayowa320@gmail.com>
2022-07-19 12:32:15 -0400

    fixed api problem

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ let resultList;
 let lat;
 let lon;
 let weatherData;
+let buttons,
+  today,
+  future,
+  todayTitle,
+  todayTemp,
+  todayGust,
+  todayHumid,
+  todayUV,
+  todayImg;
 window.onload = () => {
-  weatherAPIkey = "db3e74234e90b3ab070f5a919843e508";
-  countriesAPIkey = "328d5a3aaa25e85506ef8faa8dbdf791";
+  weatherAPIkey = "1e444e598cf0f1fb24a3724e7e1fc188";
 
   city_search_input = document.querySelector("#city_search");
   search_btn = document.querySelector("#search_btn");
@@ -22,6 +30,15 @@ window.onload = () => {
   resultList = document.querySelector("#places");
   let form = document.querySelector("#form");
   form.addEventListener("submit", formSubmit);
+  buttons = document.querySelector(".buttons-container");
+  today = document.querySelector(".today");
+  future = document.querySelector(".future");
+  todayTitle = document.querySelector("#today-title");
+  todayTemp = document.querySelector("#today-temp");
+  todayGust = document.querySelector("#today-wind-speed");
+  todayHumid = document.querySelector("#today-humidity");
+  todayUV = document.querySelector(".uv-index");
+  todayImg = document.querySelector("#today-ico");
   weatherData = {};
 };
 let formSubmit = async (e) => {
@@ -31,7 +48,7 @@ let formSubmit = async (e) => {
     await getLatLon();
     await getWeather();
   }
-  displayDetails(city);
+  displayDetails();
 };
 // city_search_input.addEventListener("change", (e) => {
 //   let value = e.target.value;
@@ -49,15 +66,73 @@ let getLatLon = async () => {
 };
 let getWeather = async () => {
   let respond = await fetch(
-    `http://www.api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${weatherAPIkey}&units=imperial&cnt=6`
+    `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${weatherAPIkey}&units=imperial&cnt=6`
   );
   let data = await respond.json();
-  // console.log(data);
   weatherData[city] = data;
-  // create a button for the current city and set an event listner to perform the display
-  // remember to create a list element to populate the buttons
+  let button = document.createElement("button");
+  button.innerHTML = city;
+  button.addEventListener("click", (event) => {
+    city = event.target.innerHTML;
+    displayDetails();
+  });
+  buttons.appendChild(button);
 };
-let displayDetails = (city) => {
+let displayDetails = () => {
   // create the UI elements and display them
   // remember to create a container in html and CSS
+  future.innerHTML = "";
+  let data = weatherData[city];
+  let list = data.list;
+  let dt = new Date(list[0].dt * 1000);
+  let dtstr = `${dt.getMonth()}/${dt.getDate()}/${dt.getFullYear()}`;
+  todayTitle.innerHTML = `${city} (${dtstr})`;
+  todayTemp.innerHTML = "Temp: " + list[0].temp.day;
+  todayGust.innerHTML = "Wind Speed: " + list[0].gust;
+  todayHumid.innerHTML = "Humidity: " + list[0].humidity;
+  let weather = list[0].weather;
+  let todayId = weather[0].icon;
+  todayImg.src = `http://openweathermap.org/img/wn/${todayId}@2x.png`;
+  //http://openweathermap.org/img/wn/10d@2x.png
+
+  // loop through remaining values.
+  for (let i = 1; i < list.length; i++) {
+    let data = weatherData[city];
+    let dt = new Date(list[i].dt * 1000);
+    let dtstr = `${dt.getMonth()}/${dt.getDate()}/${dt.getFullYear()}`;
+
+    let title = document.createElement("h3");
+    title.classList.add("title");
+    let temp = document.createElement("p");
+    temp.classList.add("temp");
+    let gust = document.createElement("p");
+    gust.classList.add("wind-speed");
+    let humid = document.createElement("p");
+    humid.classList.add("humidity");
+    let iconImg = document.createElement("img");
+    iconImg.classList.add("icon");
+
+    title.innerHTML = `${city} (${dtstr})`;
+    temp.innerHTML = "Temp: " + list[i].temp.day;
+    gust.innerHTML = "Wind Speed: " + list[i].gust;
+    humid.innerHTML = "Humidity: " + list[i].humidity;
+    weather = list[0].weather;
+    let id = weather[0].icon;
+    iconImg.src = `http://openweathermap.org/img/wn/${id}@2x.png`;
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.appendChild(title);
+    card.appendChild(iconImg);
+    card.appendChild(temp);
+    card.appendChild(gust);
+    card.appendChild(humid);
+    card.style.border = "1px solid lightgrey";
+    future.appendChild(card);
+  }
+
+  today.style.border = "1px solid lightgrey";
+
+  //get description and set icons, css.
 };
+
+// gust, humidity, city, dt, description, UV Index.
